0eee4a66fb03319f163b2c91e96adca340975380
Giaduc8289 <hungtv2012@gmail.com>
2022-03-23 06:48:31 +0700

    validate trường giá trị khuyến mại giữ lại nhận giá trị từ 0 đến giá trị tổng khuyến mại (0<=coupon_reward:discount_hold_amount<=discount_fix_amount)

--- odoo/extra-addons/sale_inheritance/models/coupon_reward.py
+++ odoo/extra-addons/sale_inheritance/models/coupon_reward.py
@@ -14,5 +14,5 @@ class CouponReward(models.Model):
     @api.constrains('discount_hold_amount')
     def _check_discount_hold_amount(self):
         for record in self:
-            if record.discount_fixed_amount < record.discount_hold_amount:
-                raise ValidationError("Discount hold amount cannot larger discount fixed amount")
+            if record.discount_hold_amount < 0 or record.discount_hold_amount > record.discount_fixed_amount:
+                raise ValidationError("Discount hold amount in 0 to {}".format(record.discount_fixed_amount))
