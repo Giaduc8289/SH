df39d020a6c7c92fdd0ad829702a5848a75c7d7b
Giaduc8289 <hungtv2012@gmail.com>
2022-03-29 13:22:26 +0700

    Tính chiết khấu cho các chương trình cấu hình chi tiết

--- odoo/extra-addons/sale_inheritance/models/sale_order.py
+++ odoo/extra-addons/sale_inheritance/models/sale_order.py
@@ -7,71 +7,17 @@ from odoo.tools.misc import formatLang
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    # def _get_reward_values_discount_fixed_amount(self, program):
-    #     total_amount = sum(self._get_base_order_lines(program).mapped('price_total'))
-    #     fixed_amount = program._compute_program_amount('discount_fixed_amount', self.currency_id)
-    #     #-----Tinh gia tri duoc chiet khau trong truong hop cau hinh chi tiet
-    #     if program.config_detail:
-    #         fixed_amount = program._compute_program_amount('discount_fixed_amount_detail', self.currency_id)
-    #     #--------------------------------------------------------------------
-    #     if total_amount < fixed_amount:
-    #         return total_amount
-    #     else:
-    #         return fixed_amount
-    #
-    # def _is_valid_partner_detail(self, partner):
-    #     if self.env['coupon.detail'].partner_ids and self.rule_partners_domain != '[]':
-    #         domain = ast.literal_eval(self.rule_partners_domain) + [('id', '=', partner.id)]
-    #         return bool(self.env['res.partner'].search_count(domain))
-    #     else:
-    #         return True
-    #
-    # def _remove_invalid_reward_lines(self):
-    #     """ Find programs & coupons that are not applicable anymore.
-    #         It will then unlink the related reward order lines.
-    #         It will also unset the order's fields that are storing
-    #         the applied coupons & programs.
-    #         Note: It will also remove a reward line coming from an archive program.
-    #     """
-    #     self.ensure_one()
-    #     order = self
-    #
-    #     applied_programs = order._get_applied_programs()
-    #     applicable_programs = self.env['coupon.program']
-    #     if applied_programs:
-    #         applicable_programs = order._get_applicable_programs() + order._get_valid_applied_coupon_program()
-    #         applicable_programs = applicable_programs._keep_only_most_interesting_auto_applied_global_discount_program()
-    #     programs_to_remove = applied_programs - applicable_programs
-    #
-    #     reward_product_ids = applied_programs.discount_line_product_id.ids
-    #     # delete reward line coming from an archived coupon (it will never be updated/removed when recomputing the order)
-    #     invalid_lines = order.order_line.filtered(lambda line: line.is_reward_line and line.product_id.id not in reward_product_ids)
-    #
-    #     if programs_to_remove:
-    #         product_ids_to_remove = programs_to_remove.discount_line_product_id.ids
-    #
-    #         if product_ids_to_remove:
-    #             # Invalid generated coupon for which we are not eligible anymore ('expired' since it is specific to this SO and we may again met the requirements)
-    #             self.generated_coupon_ids.filtered(lambda coupon: coupon.program_id.discount_line_product_id.id in product_ids_to_remove).write({'state': 'expired'})
-    #
-    #         # Reset applied coupons for which we are not eligible anymore ('valid' so it can be use on another )
-    #         coupons_to_remove = order.applied_coupon_ids.filtered(lambda coupon: coupon.program_id in programs_to_remove)
-    #         coupons_to_remove.write({'state': 'new'})
-    #
-    #         # Unbind promotion and coupon programs which requirements are not met anymore
-    #         order.no_code_promo_program_ids -= programs_to_remove
-    #         order.code_promo_program_id -= programs_to_remove
-    #
-    #         if coupons_to_remove:
-    #             order.applied_coupon_ids -= coupons_to_remove
-    #
-    #         # Remove their reward lines
-    #         if product_ids_to_remove:
-    #             invalid_lines |= order.order_line.filtered(lambda line: line.product_id.id in product_ids_to_remove)
-    #
-    #     invalid_lines.unlink()
-
-
+    def _get_reward_values_discount_fixed_amount(self, program):
+        total_amount = sum(self._get_base_order_lines(program).mapped('price_total'))
+        fixed_amount = program._compute_program_amount('discount_fixed_amount', self.currency_id)
+        #-----Tinh gia tri duoc chiet khau trong truong hop cau hinh chi tiet
+        if program.config_detail:
+            fixed_amount = program.coupon_detail_ids.filtered(lambda detail: self.partner_id in detail.partner_ids).discount_fixed_amount_detail
+        #--------------------------------------------------------------------
+        if total_amount < fixed_amount:
+            return total_amount
+        else:
+            return fixed_amount
 
 
     def _get_reward_values_discount(self, program):
