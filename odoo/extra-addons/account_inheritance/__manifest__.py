5c4ab417edf7f06fb777f629d7a2b4a31a96429e
Giaduc8289 <hungtv2012@gmail.com>
2022-04-13 09:54:20 +0700

    Hiệu chỉnh giao diện

---
+++ odoo/extra-addons/account_inheritance/__manifest__.py
@@ -0,0 +1,15 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Account inheritance",
+    "version": "15.0",
+    "author": "hungtv2012@gmail.com",
+    "sequence": "8",
+    "license": "LGPL-3",
+    "category": "Hidden",
+    "depends": ["account"],
+    "data": [
+    ],
+    "installable": True,
+    "maintainers": ["hungtv2012@gmail.com"]
+}
---
+++ odoo/extra-addons/stock_inheritance/__manifest__.py
@@ -0,0 +1,15 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Stock inheritance",
+    "version": "15.0",
+    "author": "hungtv2012@gmail.com",
+    "sequence": "8",
+    "license": "LGPL-3",
+    "category": "Hidden",
+    "depends": ["stock"],
+    "data": [
+    ],
+    "installable": True,
+    "maintainers": ["hungtv2012@gmail.com"]
+}
