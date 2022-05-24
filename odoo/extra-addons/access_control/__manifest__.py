9e5692dd3bd199e6766489208c9168e2d989acde
Hung <hungtv2012@gmail.com>
2022-05-24 17:14:22 +0700

    add menu access control report

--- odoo/extra-addons/access_control/__manifest__.py
+++ odoo/extra-addons/access_control/__manifest__.py
@@ -13,6 +13,7 @@
         'security/ir.model.access.csv',
         "data/ir_sequence.xml",
         "views/access_control_view.xml",
+        "report/access_control_report.xml"
     ],
     "installable": True,
     'application': True,
