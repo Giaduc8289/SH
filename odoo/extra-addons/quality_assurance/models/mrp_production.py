6832c1e66851cb2419c0002d4f5ea7bb7c8f33e2
Thien <kingoflegend102@gmail.com>
2022-05-30 18:31:21 +0700

    config san xuat - dang hoan thien

---
+++ odoo/extra-addons/quality_assurance/models/mrp_production.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models
+from odoo.odoo.exceptions import UserError
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    def _create_stock_moves(self, picking):
+        values = []
+        for line in self.filtered(lambda l: not l.display_type):
+            for val in line._prepare_stock_moves(picking):
+                values.append(val)
+            line.move_dest_ids.created_purchase_line_id = False
+
+        return self.env['stock.move'].create(values)
+    def _create_picking(self):
+        stock_picking = self.env['stock.picking']
+        for order in self:
+            if any([ptype in ['product', 'consu'] for ptype in order.mapped('product_id.type')]):
+                pickings = order.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+                if not pickings:
+                    res = order._get_move_finished_values()
+                    picking = stock_picking.create(res)
+                else:
+                    picking = pickings[0]
+                moves = order._create_stock_moves(picking)
+                moves = moves.filtered(lambda x: x.state not in ('done', 'cancel'))._action_confirm()
+                seq = 0
+                for move in sorted(moves, key=lambda move: move.date):
+                    seq += 5
+                    move.sequence = seq
+                moves._action_assign()
+                picking.generate_quality_alert()
+                picking.message_post_with_view('mail.message_origin_link',
+                                               values={'self': picking, 'origin': order},
+                                               subtype_id=self.env.ref('mail.mt_note').id)
+        return True
