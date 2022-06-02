e9ad4eea2cf25145c414b0d72d82803d434a7e68
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-06-02 14:15:11 +0700

    cường - yêu cầu báo giá đơn mua hàng

--- odoo/extra-addons/purchase_inheritance/__manifest__.py
+++ odoo/extra-addons/purchase_inheritance/__manifest__.py
@@ -12,7 +12,8 @@
         "security/ir.model.access.csv",
         "views/purchase_report_views.xml",
         "views/product_template_view.xml",
-        "report/report_purchase_order_view.xml"
+        "report/report_purchase_order_view.xml",
+        "views/purchase_quotation_templates.xml",
     ],
     "installable": True,
     "maintainers": ["hieutv.199x@gmail.com"]
