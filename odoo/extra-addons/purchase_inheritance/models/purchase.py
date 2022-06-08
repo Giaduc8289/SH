ba4efa9ad489cb7673aaf80f07f413e1585170a1
Hung <hungtv2012@gmail.com>
2022-06-08 16:39:29 +0700

    Lọc nhà cung cấp trong module PurchaseOrder

--- odoo/extra-addons/purchase_inheritance/models/purchase.py
+++ odoo/extra-addons/purchase_inheritance/models/purchase.py
@@ -9,6 +9,14 @@ from odoo.osv import expression
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    READONLY_STATES = {
+        'purchase': [('readonly', True)],
+        'done': [('readonly', True)],
+        'cancel': [('readonly', True)],
+    }
+    partner_id = fields.Many2one('res.partner', string='Vendor', required=True, states=READONLY_STATES, change_default=True, tracking=True,
+                                 domain="[('code', 'like', 'NCC%')]", help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
+
     @api.onchange('partner_id', 'company_id')
     def onchange_partner_id(self):
         super(PurchaseOrder, self).onchange_partner_id()
