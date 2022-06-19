93825c9aedfb31a65b1c9664c0a4dbf59c84d7ef
hungtv <hungtv2012@gmail.com>
2022-06-19 16:30:27 +0700

    Kiểm tra điều kiện áp dụng chương trình khuyến mại

--- odoo/extra-addons/sale_inheritance/models/coupon_program.py
+++ odoo/extra-addons/sale_inheritance/models/coupon_program.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import ast
 
 from odoo import fields, models, api
 from odoo.exceptions import ValidationError
@@ -10,8 +11,24 @@ class CouponProgram(models.Model):
 
     config_detail = fields.Boolean(string='Cấu hình chi tiết', default=False)
     coupon_detail_ids = fields.One2many('coupon.detail', 'program_id', string="Chiết khấu chi tiết")
+
     coupon_partner_ids = fields.Many2many('res.partner', column1='coupon_program_id', column2='code',
                                             relation='coupon_program_res_partner_rel', string="Khách hàng",
                                             domain="[('code', 'like', 'KH%')]")
     coupon_products_ids = fields.Many2many('product.template', column1='coupon_program_id', column2='id',
                                             relation='coupon_program_product_template_rel', string="Sản phẩm")
+
+    def _is_valid_partner(self, partner):
+        if self.coupon_partner_ids:
+            domain = [('id', 'in', self.coupon_partner_ids.ids), ('id', '=', partner.id)]
+            return bool(self.env['res.partner'].search_count(domain))
+        else:
+            return True
+
+    def _get_valid_products(self, products):
+        if self.coupon_products_ids:
+            domain = [('id', 'in', self.coupon_products_ids.ids)]
+            return products.filtered_domain(domain)
+        return products
+
+
