203465b7ab0357cfb2c19f0ce46e5d8f0dd8535c
Hung <hungtv2012@gmail.com>
2022-06-25 11:38:17 +0700

    Hiệu chỉnh lại Report Access control

---
+++ odoo/extra-addons/access_control/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import access_control_report
+from . import weight_vehicle_report
---
+++ odoo/extra-addons/access_control/wizard/weight_vehicle_report.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+from datetime import datetime, timedelta
+
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+
+
+class WeightVehicleReportWizard(models.TransientModel):
+    _name = 'weight.vehicle.report.wizard'
+
+    date_start = fields.Date(string='Start Date')
+    date_end = fields.Date(string='End Date')
+    purpose = fields.Selection([('sale', 'Sale'), ('purchase', 'Purchase'), ('visit', 'Visit'), ('work', 'Work')],
+                               'Purpose')
+    res_partner_id = fields.Many2one("res.partner", "Partner", domain="[('code', '!=', None)]")
+
+    def get_report(self):
+        data = {
+            'model': self._name,
+            'ids': self.ids,
+            'form': {
+                'date_start': self.date_start, 'date_end': self.date_end, 'purpose': self.purpose, 'res_partner_id': self.res_partner_id
+            },
+        }
+
+        action = self.env.ref('access_control.action_report_weight_vehicle').report_action(self, data=data)
+        return action
+
+class ReportWeightVehicle(models.AbstractModel):
+    _name = 'report.access_control.report_weight_vehicle_document'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        date_start = data['form']['date_start']
+        date_end = data['form']['date_end']
+        purpose = data['form']['purpose']
+        res_partner_id = data['form']['res_partner_id']
+
+        domain = []
+        if date_start:
+            domain = expression.AND([domain, [('in_time', '>=', date_start)]])
+        if date_end:
+            domain = expression.AND([domain, [('in_time', '<=', date_end)]])
+        if purpose:
+            domain = expression.AND([domain, [('purpose', '=', purpose)]])
+        # if res_partner_id:
+        #     domain = expression.AND([domain, [('res_partner_id.code', 'like', res_partner_id.code)]])
+        data_ac = self.env['access.control'].search(domain)
+        docs = self.env['access.control'].browse(data_ac.ids)
+
+        return {
+            'doc_ids': data['ids'],
+            'doc_model': data['model'],
+            'date_start': date_start,
+            'date_end': date_end,
+            'purpose': purpose,
+            'res_partner_id': res_partner_id,
+            'docs': docs,
+        }
+
