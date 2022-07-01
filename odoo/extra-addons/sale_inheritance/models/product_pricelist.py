a45abf3efddaf0ef9a32a1a98725a42f4a340c8a
Hung <hungtv2012@gmail.com>
2022-07-01 17:56:01 +0700

    Hiệu chỉnh Bảng giá

--- odoo/extra-addons/sale_inheritance/models/product_pricelist.py
+++ odoo/extra-addons/sale_inheritance/models/product_pricelist.py
@@ -5,12 +5,13 @@ from odoo import fields, models, api
 from odoo.tools import populate
 from datetime import timedelta, date
 
+
 class Pricelist(models.Model):
     _inherit = "product.pricelist"
 
     compute_price = fields.Selection([
         ('fixed', 'Fixed Price'),
-        ('percentage', 'Discount'),
+        # ('percentage', 'Discount'),
         ('formula', 'Formula')], index=True, default='fixed', required=True)
     base = fields.Selection([
         ('list_price', 'Sales Price'),
@@ -23,9 +24,10 @@ class Pricelist(models.Model):
              'Other Pricelist : Computation of the base price based on another Pricelist.')
     base_pricelist_id = fields.Many2one('product.pricelist', 'Other Pricelist', check_company=True)
     date_start = fields.Datetime('Start Date', help="Starting datetime for the pricelist item validation\n"
-                                                "The displayed value depends on the timezone set in your preferences.")
+                                                    "The displayed value depends on the timezone set in your preferences.")
     date_end = fields.Datetime('End Date', help="Ending datetime for the pricelist item validation\n"
                                                 "The displayed value depends on the timezone set in your preferences.")
+
 
 class PricelistItem(models.Model):
     _inherit = "product.pricelist.item"
@@ -37,4 +39,23 @@ class PricelistItem(models.Model):
     support_shipping = fields.Float('Support Shipping')
     support_new_agency = fields.Float('Support New Agency')
     reduce_loss = fields.Float('Reduce Loss')
+    fixed_price = fields.Float('Fixed Price', digits='Product Price')
+    final_price = fields.Float('Final Price', compute='_compute_final_price')
+
+    @api.onchange('product_tmpl_id')
+    def product_tmpl_id_change(self):
+        if not self.product_tmpl_id:
+            return
+        if self.pricelist_id.base_pricelist_id:
+            data = self.env['product.pricelist.item'].search([('pricelist_id', '=', self.pricelist_id.base_pricelist_id.ids)
+                                                                 , ('product_tmpl_id', '=', self.product_tmpl_id.id)], limit=1)
+            if data:
+                vals = {}
+                vals['fixed_price'] = data[0].fixed_price
+                self.update(vals)
+
+    @api.onchange('fixed_price','discount_month','discount_year','reduce_direct','bonus_quantity','support_shipping','support_new_agency','reduce_loss')
+    def _compute_final_price(self):
+        for item in self:
+            item.final_price = item.fixed_price - item.discount_month - item.discount_year - item.reduce_direct - item.bonus_quantity - item.support_shipping - item.support_new_agency - item.reduce_loss
 
--- odoo/extra-addons/sale_inheritance/models/sale_order_line.py
+++ odoo/extra-addons/sale_inheritance/models/sale_order_line.py
@@ -18,3 +18,21 @@ class SaleOrderLine(models.Model):
                 record.amount_package = 0
             else:
                 record.product_uom_qty = int(record.amount_package * record.product_weight)
+
+    def _get_display_price(self, product):
+        pricelist_item = self.order_id.pricelist_id.id
+        # str = pricelist_item.ids[product.product_id.id]
+        data = self.env['product.pricelist.item'].search([('pricelist_id', '=', pricelist_item)
+                                                             , ('product_tmpl_id', '=', product.id)], limit=1)
+        if data:
+            base_price, currency = data[0].fixed_price, data[0].currency_id#.base_pricelist_id.currency_id
+            final_price = data[0].final_price
+        #
+        # final_price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(product or self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
+        # base_price, currency = self.with_context(product_context)._get_real_price_currency(product, rule_id, self.product_uom_qty, self.product_uom, self.order_id.pricelist_id.id)
+        # if currency != self.order_id.pricelist_id.currency_id:
+        #     base_price = currency._convert(
+        #         base_price, self.order_id.pricelist_id.currency_id,
+        #         self.order_id.company_id or self.env.company, self.order_id.date_order or fields.Date.today())
+        # # negative discounts (= surcharge) are included in the display price
+        return max(base_price, final_price)
