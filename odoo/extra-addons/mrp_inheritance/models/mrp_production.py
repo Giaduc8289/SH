03558ef7101f0876993a77243d6afe5b99b64d27
Hung <hungtv2012@gmail.com>
2022-07-11 14:05:36 +0700

    Xử lý lọc sản phẩm trong mrp_inheritance

--- odoo/extra-addons/mrp_inheritance/models/mrp_production.py
+++ odoo/extra-addons/mrp_inheritance/models/mrp_production.py
@@ -5,5 +5,4 @@ from odoo import api, fields, models, _
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    product_id = fields.Many2one("product.product", "Product",
-                                 domain="[('default_code', 'not like', 'BCA%'), '|', '|', ('default_code', 'like', 'CAVI%'), ('default_code', 'like', 'BN%'),('default_code', 'like', 'BC%')]")
+    product_id = fields.Many2one("product.product", "Product", domain="[('categ_id', 'in', (10, 11, 12))]")
