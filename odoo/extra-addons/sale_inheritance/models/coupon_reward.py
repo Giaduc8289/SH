187dc233ba77caddf123070deb426df061be72e2
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-07-13 17:34:38 +0700

    Linh - tính toán chiết khấu trả sau

--- odoo/extra-addons/sale_inheritance/models/coupon_reward.py
+++ odoo/extra-addons/sale_inheritance/models/coupon_reward.py
@@ -9,11 +9,5 @@ class CouponReward(models.Model):
     _description = "Coupon Reward"
 
     discount_for_amount = fields.Boolean(string='Discount for amount', default=True)
-    discount_hold_amount = fields.Float(string='Discount hold amount', default=0)
     discount_hold_time = fields.Selection([('month', 'Month'), ('quarter', 'Quarter'), ('year', 'Year')], 'Discount hold time', default='year')
 
-    @api.constrains('discount_hold_amount')
-    def _check_discount_hold_amount(self):
-        for record in self:
-            if record.discount_hold_amount < 0 or record.discount_hold_amount > record.discount_fixed_amount:
-                raise ValidationError("Discount hold amount in 0 to {}".format(record.discount_fixed_amount))
