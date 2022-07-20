85dce963107f8143ebf84c46e0a9336a2f930f67
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-07-20 11:46:59 +0700

    Linh - đổi nút báo cáo mua hàng tổng hợp, check out chỉ khi đã cân ra

--- odoo/extra-addons/access_control/models/access_control.py
+++ odoo/extra-addons/access_control/models/access_control.py
@@ -136,7 +136,7 @@ class AccessControl(models.Model):
 
     def action_check_out(self):
         for record in self:
-            if (record.state != 'out'):
+            if (record.state == 'weighout'):
                 record.out_time = fields.Datetime.now()
                 record.state = 'out'
 
