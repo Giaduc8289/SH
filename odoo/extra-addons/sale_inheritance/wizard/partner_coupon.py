8e53d45c13466a099872289fded78081896b03bb
Hung <hungtv2012@gmail.com>
2022-07-21 14:46:59 +0700

    Bangr gia chiet khau theo khachs

--- odoo/extra-addons/sale_inheritance/wizard/partner_coupon.py
+++ odoo/extra-addons/sale_inheritance/wizard/partner_coupon.py
@@ -1,9 +1,11 @@
+from operator import itemgetter
+
 from odoo import models, fields, api
 
 from datetime import datetime, timedelta
 
 from odoo.osv import expression
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT, groupby
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
 class PartnerCouponWizard(models.TransientModel):
@@ -37,20 +39,47 @@ class PartnerCouponWizard(models.TransientModel):
     def get_report(self):
         code = self.khachhang.code
 
-        # query = """
-        # SELECT cp.name as name_coupon,  product_template.name as product_coupon, fixed_price
-        #     from coupon_program cp join coupon_program_product_template_rel cp_pt_rel on cp.id = cp_pt_rel.coupon_program_id
-        #     join coupon_program_res_partner_rel on cp.id = coupon_program_res_partner_rel.coupon_program_id
-        #     join product_pricelist_item on cp_pt_rel.id = product_pricelist_item.product_tmpl_id
-        #     join res_partner ON res_partner.id = coupon_program_res_partner_rel.code
-        #     join coupon_reward on coupon_reward.id = cp.reward_id
-        #     join product_template on product_template.id = cp_pt_rel.id
-        #     where res_partner.code=%s
-        # """
-        #
-        # self.env.cr.execute(query, (code,))
-        # data_sa = self.env.cr.fetchall()
-        # docs = self.env['partner.coupon.wizard'].browse(data_sa)
+        query = """
+        SELECT cp.name as name_coupon,  product_template.name as product_coupon, fixed_price
+            from coupon_program cp join coupon_program_product_template_rel cp_pt_rel on cp.id = cp_pt_rel.coupon_program_id
+            join coupon_program_res_partner_rel on cp.id = coupon_program_res_partner_rel.coupon_program_id
+            join product_pricelist_item on cp_pt_rel.id = product_pricelist_item.product_tmpl_id
+            join res_partner ON res_partner.id = coupon_program_res_partner_rel.code
+            join coupon_reward on coupon_reward.id = cp.reward_id
+            join product_template on product_template.id = cp_pt_rel.id
+            where res_partner.code=%s
+        """
+
+        self.env.cr.execute(query, (code,))
+        data_sa = self.env.cr.fetchall()
+        # tính toán ra 1 data_wizard
+        # danh sách sản phẩm ở trong data_sa
+        # values = set(map(lambda x: x[1], data_sa))
+        # data_sp = [[y[0] for y in data_sa if y[1] == x] for x in values]
+        data_sa.sort(key=itemgetter(1))
+        data_sp = groupby(data_sa, itemgetter(1))
+        # data_sp = data_sa.read_group(fields=['product_coupon'], groupby=['product_coupon'])
+        data_final = []
+        # for sp in data_sp:
+        #     #tao moi dong du lieu cho san pham
+        #     data_row=[]
+        #     data_row.append({'product_coupon': sp[0]})
+        #     for ct in sp[1]:
+        #         data_row.append({
+        #             ct[0]: ct[2],
+        #         })
+        #     data_final.append(data_row)
+        for sp in data_sp:
+            #tao moi dong du lieu cho san pham
+            for ct in sp[1]:
+                data_row = ({
+                    'product_coupon': sp[0],
+                    'name_coupon': ct[0],
+                    'fixed_price': ct[2],
+                })
+                data_final.append(data_row)
+
+        docs = self.env['partner.coupon.wizard'].browse(data_final)
 
         data = {
             'model': self._name,
@@ -82,15 +111,26 @@ class DiscountCouponReport(models.AbstractModel):
         """
         self.env.cr.execute(query, (code,))
         data_sa = self.env.cr.fetchall()
-        #tính toán ra 1 data_wizard
-        docs = self.env['partner.coupon.wizard'].browse(data_sa)
-        data_doc = docs.create(data_sa)
+        # tính toán ra 1 data_wizard
+        # danh sách sản phẩm ở trong data_sa
+        data_sa.sort(key=itemgetter(1))
+        data_sp = groupby(data_sa, itemgetter(1))
+        data_final = []
+        for sp in data_sp:
+            for ct in sp[1]:
+                data_row = ({
+                    'product_coupon': sp[0],
+                    'name_coupon': ct[0],
+                    'fixed_price': ct[2],
+                })
+                data_final.append(data_row)
+
+        docs = self.env['partner.coupon.wizard'].browse(data_final)
 
         return {
             # 'doc_ids': data['ids'],
-            'doc_model': data['model'],
+            'doc_model': 'partner.coupon.wizard',
             'khachhang': khachhang,
             'docs': docs,
-            'data_doc': data_doc
         }
 
