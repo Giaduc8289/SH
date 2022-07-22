660d007341a11c55521a42632f45aa164b51e05b
Hung <hungtv2012@gmail.com>
2022-07-22 16:14:22 +0700

    Bảng giá sau chiết khấu

--- odoo/extra-addons/sale_inheritance/wizard/partner_coupon.py
+++ odoo/extra-addons/sale_inheritance/wizard/partner_coupon.py
@@ -10,44 +10,28 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
 class PartnerCouponWizard(models.TransientModel):
     _name = 'partner.coupon.wizard'
-    _rec_name = 'khachhang'
+    _rec_name = 'res_partner_id'
 
     product_pricelist_id = fields.Many2one('product.pricelist', 'Bảng giá', required=True)
-    khachhang = fields.Many2one("res.partner", "Khách hàng", domain="[('code', 'like', 'KH%')]", required=True)
-    name_coupon = fields.Many2one("coupon.program", "Tên chương trình", readonly=True, store=True)
-    product_coupon = fields.Many2one("product.template", "Tên sản phẩm", readonly=True, store=True)
-    fixed_price = fields.Many2one("product.pricelist.item", "Giá sản phẩm", readonly=True, store=True)
-    discount_percentage = fields.Many2one("coupon.reward", "Phần trăm giảm", readonly=True, store=True)
-    discount_hold_time = fields.Many2one("coupon.reward", "Thời gian hoàn tiền", readonly=True, store=True)
-    payment_type = fields.Many2one("coupon.program", "Kiểu thanh toán", readonly=True, store=True)
-    discount_type = fields.Many2one("coupon.reward", "Kiểu chương trình", readonly=True, store=True)
-    pricelist_id = fields.Many2one(
-        'product.pricelist', string='Pricelist', check_company=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", tracking=1,
-        help="If you change the pricelist, only newly added lines will be affected.", store=True)
-    currency_id = fields.Many2one(related='pricelist_id.currency_id', depends=["pricelist_id"], store=True, ondelete="restrict")
-    # amount_coupon = fields.Monetary(string='Price Amount', compute='_amount_coupon_price')
-    #
-    # def _amount_coupon_price(self):
-    #     coupon_price = 0.0
-    #     for record in self:
-    #         if(record.discount_type == 'percentage'):
-    #             coupon_price = record.fixed_price - (record.fixed_price * record.discount_percentage / 100)
-    #         record.update({
-    #             'amount_coupon': coupon_price,
-    #         })
+    res_partner_id = fields.Many2one("res.partner", "Khách hàng", domain="[('code', 'like', 'KH%')]", required=True)
+
+    product_tmpl_id = fields.Many2one("product.template", "Product Template", readonly=True, store=True)
+    product_name = fields.Char("Tên sản phẩm", readonly=True, store=True)
+    coupon_name = fields.Char("Tên chương trình", readonly=True, store=True)
+    fixed_price = fields.Float("Giá sản phẩm", readonly=True, store=True)
+    discount_fixed_amount = fields.Float("Giá trị chiết khấu", readonly=True, store=True)
 
     def get_report(self):
-        pricelist = self.product_pricelist_id
-        code = self.khachhang.code
+        pricelist = self.product_pricelist_id.id
+        code = self.res_partner_id.code
 
         query = """
-            select product_tmpl_id, namesp, idkm, namekm, discount_fixed_amount  
+            select product_tmpl_id, product_name, fixed_price, coupon_id, coupon_name, discount_fixed_amount  
             from (
-                select sp.id, sp.name namesp, sp.categ_id
+                select sp.id, sp.name product_name, sp.categ_id
                     , bg.product_tmpl_id, bg.fixed_price
-                    , kh.id, kh.code, kh.name namekh
-                    , km.id idkm, km.reward_id, km.name namekm, km.payment_type 
+                    , kh.id, kh.code, kh.name customer_name
+                    , km.id coupon_id, km.reward_id, km.name coupon_name, km.payment_type 
                     --, kmct.discount_type, kmct.discount_percentage, kmct.discount_fixed_amount, kmct.discount_hold_time
                     , case
                         when discount_type='fixed_amount' then discount_fixed_amount
@@ -58,21 +42,21 @@ class PartnerCouponWizard(models.TransientModel):
                     , coupon_program km left join coupon_reward kmct on km.reward_id = kmct.id
                         left join coupon_program_res_partner_rel kmkh on km.id=kmkh.coupon_program_id
                         left join coupon_program_product_template_rel kmsp on km.id=kmsp.coupon_program_id
-                where bg.pricelist_id=%i
+                where bg.pricelist_id=%s
                     and kh.code=%s
                     and (kmkh.code=kh.id or km.id not in (select coupon_program_id from coupon_program_res_partner_rel))
                     and (kmsp.id=sp.id or km.id not in (select coupon_program_id from coupon_program_product_template_rel))
                     and km.active
                 ) as kq	
-            Order by product_tmpl_id, idkm 
+            Order by product_tmpl_id, coupon_id 
         """
 
         self.env.cr.execute(query, (pricelist, code,))
         data_sa = self.env.cr.fetchall()
         # tính toán ra 1 data_wizard
         # danh sách sản phẩm ở trong data_sa
-        data_sa.sort(key=itemgetter(1))
-        data_sp = groupby(data_sa, itemgetter(1))
+        data_sa.sort(key=itemgetter(0))
+        data_sp = groupby(data_sa, itemgetter(0))
         # data_sp = data_sa.read_group(fields=['product_coupon'], groupby=['product_coupon'])
         data_final = []
         # for sp in data_sp:
@@ -99,8 +83,9 @@ class PartnerCouponWizard(models.TransientModel):
         data = {
             'model': self._name,
             'form': {
-                'khachhang': self.khachhang.name,
-                'code': code,
+                'customer_name': self.res_partner_id.name,
+                'pricelist': self.product_pricelist_id.id,
+                'code': self.res_partner_id.code,
             },
         }
         action = self.env.ref('sale_inheritance.action_report_discount_coupon').report_action(self, data=data)
@@ -111,16 +96,17 @@ class DiscountCouponReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        khachhang = data['form']['khachhang']
+        customer_name = data['form']['customer_name']
+        pricelist = data['form']['pricelist']
         code = data['form']['code']
 
         query = """
-            select product_tmpl_id, namesp, idkm, namekm, discount_fixed_amount  
+            select product_tmpl_id, product_name, fixed_price, coupon_id, coupon_name, discount_fixed_amount  
             from (
-                select sp.id, sp.name namesp, sp.categ_id
+                select sp.id, sp.name product_name, sp.categ_id
                     , bg.product_tmpl_id, bg.fixed_price
-                    , kh.id, kh.code, kh.name namekh
-                    , km.id idkm, km.reward_id, km.name namekm, km.payment_type 
+                    , kh.id, kh.code, kh.name customer_name
+                    , km.id coupon_id, km.reward_id, km.name coupon_name, km.payment_type 
                     --, kmct.discount_type, kmct.discount_percentage, kmct.discount_fixed_amount, kmct.discount_hold_time
                     , case
                         when discount_type='fixed_amount' then discount_fixed_amount
@@ -137,10 +123,10 @@ class DiscountCouponReport(models.AbstractModel):
                     and (kmsp.id=sp.id or km.id not in (select coupon_program_id from coupon_program_product_template_rel))
                     and km.active
                 ) as kq	
-            Order by product_tmpl_id, idkm 
+            Order by product_tmpl_id, coupon_id 
         """
 
-        self.env.cr.execute(query, (code,))
+        self.env.cr.execute(query, (pricelist, code,))
         data_sa = self.env.cr.fetchall()
         data_sa.sort(key=itemgetter(1))
         data_sp = groupby(data_sa, itemgetter(1))
@@ -159,7 +145,7 @@ class DiscountCouponReport(models.AbstractModel):
         return {
             # 'doc_ids': data['ids'],
             'doc_model': 'partner.coupon.wizard',
-            'khachhang': khachhang,
+            'customer_name': customer_name,
             'docs': docs,
         }
 
